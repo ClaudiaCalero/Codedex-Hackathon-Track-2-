a98a71b88eb890ae32a6235f5946d35af69de832
ClaudiaCalero <claudiaclrduro@gmail.com>
2024-07-12 13:54:14 +0200

    edit countdown

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ const x = setInterval(function () {
 
     const distance = countDownDate - now;
 
-    const days = Math.ceil(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60 * 60));
+    // Calcula los días, horas, minutos y segundos restantes
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    // Actualiza los elementos del HTML con los valores calculados
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
+    // Si la cuenta regresiva ha terminado, muestra un mensaje
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = "The stoop sale has started!"
+        document.getElementById("countdown").innerHTML = "The stoop sale has started!";
     }
 
 }, 1000);
